8d7ee6578f2275512644b2743f37d5a571b06aa7
RiffLord <bruno.pezer@hotmail.com>
2020-06-06 02:36:43 +0200

    Codice backend per gestione di notifiche in TeamUp

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+'use-strict'
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.sendNotification = functions.firestore.document("users/{user_doc}/notifications/{notification_doc}").onCreate((snapshot, context) => {
+    const userDoc = context.params.user_doc;    //  Riferimento al documento del recipiente
+    const notificationDoc = snapshot.data();    //  Riferimento ai dati contenuti nel documento di notifica
+
+    return admin.firestore().collection("users").doc(userDoc).get().then(tokenResult => {
+        //  Token del recipiente della notifica
+        const recepientToken = tokenResult.data().token;
+
+        //  Dati inviati nella notifica
+        const payload = {
+            notification: {
+                title: "Teammate Request",
+                body: notificationDoc.sentFrom + " has requested to join your team on " + notificationDoc.project
+            }
+        };
+
+        //  Invia la notifica all'utente con recepientToken
+        return admin.messaging().sendToDevice(recepientToken, payload).then(result => {
+            console.log("Notification sent to device " + recepientToken);
+            return null;
+        });
+    });
+});
